c26ed81c1210a4f819257c5d5c696a80733a06b6
Tim Lynch <lynch0001@gmail.com>
2020-09-23 09:51:17 -0400

    ci config update

--- src/test/java/com/lynch/dboard/DboardApplicationTests.java
+++ src/test/java/com/lynch/dboard/DboardApplicationTests.java
@@ -2,8 +2,10 @@ package com.lynch.dboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DboardApplicationTests {
 
 	@Test
